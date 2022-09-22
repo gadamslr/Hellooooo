f1221c0b1a46ff2b23949d0d1bebe011238c19fd
Gareth Adams <gadams@longroad.ac.uk>
2022-09-22 10:10:43 +0100

    Add project files.

---
+++ Selection tasks/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selection_tasks
+{
+    internal class Program
+    {
+        // Main program 
+        static void Main(string[] args)
+        {
+            string user_name;
+
+            Console.WriteLine("Hello world!");
+
+            Console.WriteLine("Please enter your name: ");
+            user_name = Console.ReadLine();  // Reads in the value entere in the console
+
+            Console.WriteLine("Hello " + user_name);
+
+            Console.ReadLine();
+        }
+    }
+}
